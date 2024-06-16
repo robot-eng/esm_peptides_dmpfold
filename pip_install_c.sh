b1f40ecadafadc989baf1b864dcee9cbf663f6f8
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2024-06-17 01:36:44 +0700

    Update pip_install_c.sh

--- pip_install_c.sh
+++ pip_install_c.sh
@@ -7,5 +7,5 @@ pip install git+https://github.com/facebookresearch/esm.git
 pip install "fair-esm[esmfold]"
 pip install modelcif
 pip install biotite
-pip install dllogger @ git+https://github.com/NVIDIA/dllogger.git
+pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
 pip install -q git+https://github.com/aqlaboratory/openfold.git@3bec3e9b2d1e8bdb83887899102eff7d42dc2ba9
