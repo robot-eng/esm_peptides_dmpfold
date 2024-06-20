8c286812016161b6b27549f14eea808bbcf0b973
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2024-06-20 19:51:25 +0700

    Create read_surface_area.py

---
+++ lib_test_write/read_surface_area.py
@@ -0,0 +1,27 @@
+from pepdata import amino_acid_properties
+
+class ProteinAnalysis:
+    def __init__(self):
+        self.list_amino = []
+        self.list_surface_area = []
+        self.dict_surface_area = {}
+        
+    def p_line_sp(self, seq):
+        for x in seq:
+            self.surface_area = amino_acid_properties.accessible_surface_area.get(x, "Unknown")
+            print(f'Amino acid: {x} and Surface area: {self.surface_area}')
+        return "-----------end-----------------"
+
+    def list_sp(self, seq):
+        self.list_amino = []
+        self.list_surface_area = []
+        for x in seq:
+            self.list_amino.append(x)
+            self.list_surface_area.append(amino_acid_properties.accessible_surface_area.get(x, "Unknown"))
+        return f'Amino_acids: {self.list_amino}, Surface_areas: {self.list_surface_area}'
+    
+    def dictv_sp(self, seq):
+        self.dict_surface_area = {}
+        for x in seq:
+            self.dict_surface_area[x] = amino_acid_properties.accessible_surface_area.get(x, "Unknown")
+        return self.dict_surface_area
