f6bf77c209158466ae069f06a720c4737e9c9cd6
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2024-06-20 22:46:27 +0700

    Update check_acid.py

--- lib_test_write/check_acid.py
+++ lib_test_write/check_acid.py
@@ -1,8 +1,9 @@
-import chou_fasman
+import pepdata as pd
+combined_dict = {key: list(pd.amino_acid_letter_indices.keys())[value] for key, value in pd.amino_acid_name_indices.items()}
 def decode_amino_acids(code):
     decoded = []
     for symbol in code:
-        for name, symbol_match in chou_fasman.amino_acid_name_indices.items():
+        for name, symbol_match in combined_dict.items():
             if symbol == symbol_match:
                 decoded.append(name)
                 break
