9a61877fc0e179080c99436bccb8d92e11430bba
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2024-06-20 19:51:17 +0700

    Update pip_install_c.sh

--- pip_install_c.sh
+++ pip_install_c.sh
@@ -9,3 +9,4 @@ pip install modelcif
 pip install biotite
 pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
 pip install -q git+https://github.com/aqlaboratory/openfold.git@3bec3e9b2d1e8bdb83887899102eff7d42dc2ba9
+pip install pepdata
